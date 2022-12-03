b81d6e1a6d24aaa156ba916e09e7a0c3e357410b
Kojo Patrick <patrickasare84@gmail.com>
2022-12-03 19:41:42 +0000

    update env

--- pages/api/auth/login.jsx
+++ pages/api/auth/login.jsx
@@ -3,8 +3,8 @@ import { Redis } from "@upstash/redis";
 import getIp from "lib/get-ip";
 
 const redis = new Redis({
-	url: process.env.UPSTASH_REDIS_REST_URL,
-	token: process.env.UPSTASH_REDIS_REST_TOKEN,
+	url: process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL,
+	token: process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_TOKEN,
 });
 
 // Create a new ratelimiter,Create a new bucket,
